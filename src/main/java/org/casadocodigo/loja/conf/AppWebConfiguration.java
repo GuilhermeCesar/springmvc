ec6ca8c5400667fe8f1de64d2d45a2a8fb095abc
guilherme <gui11_3@hotmail.com>
2019-01-21 22:41:31 -0200

    Criate Config class

---
+++ src/main/java/org/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -0,0 +1,10 @@
+package org.casadocodigo.loja.conf;
+
+import org.casadocodigo.loja.controllers.HomeController;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ComponentScan(basePackageClasses = HomeController.class)
+public class AppWebConfiguration {
+}
