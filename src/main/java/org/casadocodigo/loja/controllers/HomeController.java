234c49c8183bdb9fc57299da8b6066b404ac1c3d
guilherme <gui11_3@hotmail.com>
2019-02-24 19:38:32 -0300

    Criando nova tela inicial

--- src/main/java/org/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/org/casadocodigo/loja/controllers/HomeController.java
@@ -1,15 +1,27 @@
 package org.casadocodigo.loja.controllers;
 
+import org.casadocodigo.loja.daos.ProdutoDao;
+import org.casadocodigo.loja.models.Produto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class
 HomeController {
 
+    @Autowired
+    private ProdutoDao produtoDao;
+
     @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String index(){
-        return "home";
+    public ModelAndView index(){
+        List<Produto> produtos = this.produtoDao.listar();
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("produtos",produtos);
+        return modelAndView;
     }
 }
