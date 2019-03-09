a4288ff41d9a2d442fa4a9f1200da0d2c0629553
guilherme <gui11_3@hotmail.com>
2019-03-08 21:43:31 -0300

    Adicionando regras de acesso no spring security

--- src/main/java/org/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/org/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,8 +1,26 @@
 package org.casadocodigo.loja.conf;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/resources/**").permitAll()
+                .antMatchers("/carrinho/**").permitAll()
+                .antMatchers("/pagamentos/**").permitAll()
+                .antMatchers("/produtos/form").hasRole("ADMIN")
+                .antMatchers("/carrinho").permitAll()
+                .antMatchers(HttpMethod.POST,"/produtos").hasRole("ADMIM")
+                .antMatchers(HttpMethod.GET,"/produtos").hasRole("ADMIN")
+                .antMatchers("/produtos/**").permitAll()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
+    }
 }
